db769369e3fb2c5924664b1cc69b1171511af14b
Yoojin99 <41438361+Yoojin99@users.noreply.github.com>
2021-01-21 14:36:54 +0900

    Update 'uploadFile()' to use responseEntity
    1. Use responseEntity to return upload result.

--- mreview/src/main/java/org/zerock/mreview/controller/UploadController.java
+++ mreview/src/main/java/org/zerock/mreview/controller/UploadController.java
@@ -2,16 +2,26 @@ package org.zerock.mreview.controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.zerock.mreview.dto.UploadResultDTO;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -21,13 +31,29 @@ public class UploadController {
     @Value("${org.zerock.upload.path}")
     private String uploadPath;
 
-    @PostMapping("/uploadAjax")
-    public void uploadFile(MultipartFile[] uploadFiles){
-        for (MultipartFile uploadFile : uploadFiles) {
+    private String makeFolder() {
+        String str = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
 
-            if(uploadFile.getContentType().startsWith("image") == false){
+        String folderPath = str.replace("//", File.separator);
+
+        File uploadPathFolder = new File(uploadPath, folderPath);
+
+        if(uploadPathFolder.exists() == false) {
+            uploadPathFolder.mkdirs();
+        }
+
+        return folderPath;
+    }
+
+    @PostMapping("/uploadAjax")
+    public ResponseEntity<List<UploadResultDTO>> uploadFile(MultipartFile[] uploadFiles) {
+
+        List<UploadResultDTO> resultDTOList = new ArrayList<>();
+
+        for (MultipartFile uploadFile : uploadFiles) {
+            if (uploadFile.getContentType().startsWith("image") == false) {
                 log.warn("this file is not image type");
-                return;
+                return new ResponseEntity<>(HttpStatus.FORBIDDEN);
             }
 
             String originalName = uploadFile.getOriginalFilename();
@@ -43,25 +69,39 @@ public class UploadController {
 
             Path savePath = Paths.get(saveName);
 
-            try{
+            try {
                 uploadFile.transferTo(savePath);
+                resultDTOList.add(new UploadResultDTO(fileName, uuid, folderPath));
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+        return new ResponseEntity<>(resultDTOList, HttpStatus.OK);
     }
 
-    private String makeFolder() {
-        String str = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+    @GetMapping("/display")
+    public ResponseEntity<byte[]> getFile(String fileName) {
+        ResponseEntity<byte[]> result = null;
 
-        String folderPath = str.replace("//", File.separator);
+        try {
+            String srcFileName = URLDecoder.decode(fileName, "UTF-8");
 
-        File uploadPathFolder = new File(uploadPath, folderPath);
+            log.info("fileName: "+srcFileName);
 
-        if(uploadPathFolder.exists() == false) {
-            uploadPathFolder.mkdirs();
+            File file = new File(uploadPath + File.separator + srcFileName);
+
+            log.info("file: "+file);
+
+            HttpHeaders header = new HttpHeaders();
+
+            header.add("Content-Type", Files.probeContentType(file.toPath()));
+
+            result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-
-        return folderPath;
+        return result;
     }
+
 }
