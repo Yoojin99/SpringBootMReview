42bcd8ff618c5c7c77c0a1f73771fdae5cc5dd56
Yoojin99 <41438361+Yoojin99@users.noreply.github.com>
2021-01-22 12:29:55 +0900

    Enable getting original image file when clicking thumbnail

--- mreview/src/main/java/org/zerock/mreview/controller/UploadController.java
+++ mreview/src/main/java/org/zerock/mreview/controller/UploadController.java
@@ -90,7 +90,7 @@ public class UploadController {
     }
 
     @GetMapping("/display")
-    public ResponseEntity<byte[]> getFile(String fileName) {
+    public ResponseEntity<byte[]> getFile(String fileName, String size) {
         ResponseEntity<byte[]> result = null;
 
         try {
@@ -99,6 +99,10 @@ public class UploadController {
             log.info("fileName: "+srcFileName);
 
             File file = new File(uploadPath + File.separator + srcFileName);
+
+            if(size != null && size.equals("1")){
+                file = new File(file.getParent(), file.getName().substring(2));
+            }
 
             log.info("file: "+file);
 
